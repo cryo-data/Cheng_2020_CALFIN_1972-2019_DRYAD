76bc765f05443494f8fa2ccec59dea3b9a1abe59
Kenneth D. Mankoff <mankoff@gmail.com>
2022-06-03 09:00:01 -0700

    cryo data folder

---
+++ cryo-data/download.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+datalad download-url --nosave \
+	https://datadryad.org/stash/downloads/file_stream/458635 \
+	https://datadryad.org/stash/downloads/file_stream/458628 \
+	https://datadryad.org/stash/downloads/file_stream/458634 \
+	https://datadryad.org/stash/downloads/file_stream/458627 \
+	https://datadryad.org/stash/downloads/file_stream/458633 \
+	https://datadryad.org/stash/downloads/file_stream/458632 \
+	https://datadryad.org/stash/downloads/file_stream/458631 \
+	https://datadryad.org/stash/downloads/file_stream/458630 \
+	https://datadryad.org/stash/downloads/file_stream/458629
